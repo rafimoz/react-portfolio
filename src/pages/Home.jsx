ba22cac820b3eba19f3fba5520da558f1cac4712
Rafi Mozumder <rafimozumder@gmail.com>
2024-12-06 03:43:59 +0600

    new link curved text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,51 +40,51 @@ const Home = () => {
   const navRef = useRef(null);
 
 
-    useEffect(() => {
-      const animateDiv = (element) => {
-        if (!element) return;
-  
-        const elementSize = 288; // Width and height of the div (72rem = 288px)
-        const randomPosition = () => ({
-          x: gsap.utils.random(0, window.innerWidth - elementSize, true),
-          y: gsap.utils.random(0, window.innerHeight - elementSize, true),
-        });
-  
-        const animate = () => {
-          gsap.to(element, {
-            ...randomPosition(),
-            duration: gsap.utils.random(3, 6, true),
-            ease: 'power1.inOut',
-            onComplete: animate,
-          });
-        };
-  
-        animate();
-      };
-  
-      animateDiv(div1Ref.current);
-      animateDiv(div2Ref.current);
-  
-      return () => {
-        gsap.killTweensOf(div1Ref.current);
-        gsap.killTweensOf(div2Ref.current);
-      };
-    }, []);
+  useEffect(() => {
+    const animateDiv = (element) => {
+      if (!element) return;
 
-  
+      const elementSize = 288; // Width and height of the div (72rem = 288px)
+      const randomPosition = () => ({
+        x: gsap.utils.random(0, window.innerWidth - elementSize, true),
+        y: gsap.utils.random(0, window.innerHeight - elementSize, true),
+      });
+
+      const animate = () => {
+        gsap.to(element, {
+          ...randomPosition(),
+          duration: gsap.utils.random(3, 6, true),
+          ease: 'power1.inOut',
+          onComplete: animate,
+        });
+      };
+
+      animate();
+    };
+
+    animateDiv(div1Ref.current);
+    animateDiv(div2Ref.current);
+
+    return () => {
+      gsap.killTweensOf(div1Ref.current);
+      gsap.killTweensOf(div2Ref.current);
+    };
+  }, []);
+
+
   const curvedTextRef = useRef(null);
   useGSAP(() => {
     const tl = gsap.timeline({ repeat: -1 });
     tl.to(curvedTextRef.current, {
-      scale:1,
+      scale: 1,
       duration: 0.6,
       ease: 'power1.inOut',
     }),
-    tl.to(curvedTextRef.current, {
-      rotation: 360,
-      duration: 10,
-      ease: "linear",
-    });
+      tl.to(curvedTextRef.current, {
+        rotation: 360,
+        duration: 10,
+        ease: "linear",
+      });
 
     curvedTextRef.current.addEventListener('mouseenter', () => {
       gsap.to(tl, { timeScale: 0.2, duration: 0.5 });
@@ -113,7 +113,7 @@ const Home = () => {
       duration: 1.5, // Animation duration
       delay: 0.8,
       ease: 'power3.out', // Smooth easing
-      
+
     })
   }, []);
 
@@ -141,24 +141,16 @@ const Home = () => {
       <div className='w-full relative h-[90vh] sm:h-[100vh] overflow-hidden backdrop-filter backdrop-blur-2xl'>
         <div className='absolute z-10 bottom-0 w-full h-1/4 bg-gradient-to-t from-secondary to-transparent'></div>
         <div className='sm:w-1/3 w-3/4 absolute bottom-0 sm:-right-2 -right-12 translate-x-52 opacity-0' ref={headerImg}>
-         {/* <img className='w-full' src="src\assets\header-image.png"  alt=""/> */} 
-
-
-<a href="https://freeimage.host/"><img className='w-full'   src="https://iili.io/2EQv5ge.png" alt="2EQv5ge.png" border="0" /></a>
-
-
-
-
-
+          <img className='w-full' src="https://iili.io/2EQv5ge.png" alt="2EQv5ge.png" border="0" />
         </div>
         <div className='absolute flex items-center flex-col z-20 bottom-0 w-full opacity-0 translate-y-32' ref={headerRef}>
           <div>
-          <p className='w-full text-[13vw] font-allura leading-none'>Rafi</p>
-          <p className='w-full text-[17.5vw] font-bodoni leading-none'>MOZUMDER</p>
+            <p className='w-full text-[13vw] font-allura leading-none'>Rafi</p>
+            <p className='w-full text-[17.5vw] font-bodoni leading-none'>MOZUMDER</p>
           </div>
         </div>
         <div className=' scale-0 absolute z-20 top-0 right-0' ref={curvedTextRef} >
-          <img className='scale-75 cursor-pointer rounded-full' src="src\assets\header-curved-text.svg" alt="" />
+          <img className='scale-75 cursor-pointer rounded-full' src="https://iili.io/2EDOBuj.png" alt="" />
         </div>
 
         <div ref={navRef} className='absolute z-20 text-lg left-5 top-[-100px] flex flex-col sm:flex-row font-aboreto'>
